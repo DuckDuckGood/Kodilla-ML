8ed9e545aba43a9eab32fe40cae99b02fe0543fe
Johnny <karoldziedziel@duck.com>
2024-11-12 17:27:33 +0100

    2_1

---
+++ 2_1/collections.py
@@ -0,0 +1,19 @@
+someList = [1, "2", "Three", 4]
+someList.append("Five")
+print(someList)
+
+someTuple = (1, "2", "Three", 4)
+print(someTuple)
+
+someSet = {"1", "2", "Three", 4}
+someSet.add("Five")
+print(someSet)
+
+someDict = {
+    "Uno": "1",
+    "Dos": "2",
+    "Tres": 3,
+    4: ["4"]
+}
+someDict["Five"] = "V"
+print(someDict)
